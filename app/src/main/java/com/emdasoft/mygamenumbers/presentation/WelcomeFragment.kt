00fdca0fe33ab4f6267414518642161268d5f803
GesserWy <emdasoftware@gmail.com>
2023-03-27 20:03:16 +0300

    add_second_fragment

--- app/src/main/java/com/emdasoft/mygamenumbers/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/emdasoft/mygamenumbers/presentation/WelcomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.emdasoft.mygamenumbers.R
 import com.emdasoft.mygamenumbers.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
@@ -19,6 +20,16 @@ class WelcomeFragment : Fragment() {
     ): View {
         _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.goButton.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.main_container, ChooseLevelFragment.newInstance())
+                .addToBackStack(null)
+                .commit()
+        }
     }
 
     override fun onDestroyView() {

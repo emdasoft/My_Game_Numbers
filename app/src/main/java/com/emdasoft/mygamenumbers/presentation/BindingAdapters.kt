65906659b3a078cee3f5a206e7f0b229674f42b7
GesserWy <emdasoftware@gmail.com>
2023-03-30 13:44:30 +0300

    add_dataBinding

--- app/src/main/java/com/emdasoft/mygamenumbers/presentation/BindingAdapters.kt
+++ app/src/main/java/com/emdasoft/mygamenumbers/presentation/BindingAdapters.kt
@@ -1,10 +1,15 @@
 package com.emdasoft.mygamenumbers.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.emdasoft.mygamenumbers.R
 import com.emdasoft.mygamenumbers.domain.entity.GameResult
+import com.emdasoft.mygamenumbers.domain.entity.Question
 
 @BindingAdapter("requiredAnswer")
 fun bindRequiredAnswer(textView: TextView, count: Int) {
@@ -43,6 +48,28 @@ fun bindImageSmile(imageView: ImageView, winner: Boolean) {
     imageView.setImageResource(getSmileResId(winner))
 }
 
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(textView: TextView, number: Int) {
+    textView.text = number.toString()
+}
+
+@BindingAdapter("barAnswersProgress")
+fun bindBarAnswersProgress(progressBar: ProgressBar, count: Int) {
+    progressBar.setProgress(count, true)
+}
+
+@BindingAdapter("enoughPercent")
+fun bindEnoughPercent(progressBar: ProgressBar, state: Boolean) {
+    val color = getColorByState(progressBar.context, state)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+}
+
+@BindingAdapter("enoughCount")
+fun bindEnoughCount(textView: TextView, state: Boolean) {
+    val color = getColorByState(textView.context, state)
+    textView.setTextColor(color)
+}
+
 private fun getPercentOfRightAnswers(result: GameResult) = with(result) {
     if (countOfQuestions == 0) {
         0
@@ -52,9 +79,18 @@ private fun getPercentOfRightAnswers(result: GameResult) = with(result) {
 }
 
 private fun getSmileResId(winner: Boolean): Int {
-    return if(winner) {
+    return if (winner) {
         R.drawable.fun_smile
     } else {
         R.drawable.sad_smile
     }
+}
+
+private fun getColorByState(context: Context, state: Boolean): Int {
+    val colorResId = if (state) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorResId)
 }
--- app/src/main/java/com/emdasoft/mygamenumbers/presentation/GameFragment.kt
+++ app/src/main/java/com/emdasoft/mygamenumbers/presentation/GameFragment.kt
@@ -1,12 +1,10 @@
 package com.emdasoft.mygamenumbers.presentation
 
-import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -53,41 +51,22 @@ class GameFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        bindViewModel()
+
         setOptionsClickListeners()
 
         viewModelObserve()
 
     }
 
+    private fun bindViewModel() {
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+    }
+
     private fun viewModelObserve() {
         viewModel.question.observe(viewLifecycleOwner) {
             showQuestion(it)
-        }
-
-        viewModel.progressAnswers.observe(viewLifecycleOwner) {
-            binding.tvAnswersProgress.text = it
-        }
-
-        viewModel.formattedTime.observe(viewLifecycleOwner) {
-            binding.tvTimer.text = it
-        }
-
-        viewModel.percentOfRightAnswers.observe(viewLifecycleOwner) {
-            binding.barAnswersProgress.setProgress(it, true)
-        }
-
-        viewModel.enoughCount.observe(viewLifecycleOwner) {
-            val color = getColorByState(it)
-            binding.tvAnswersProgress.setTextColor(color)
-        }
-
-        viewModel.enoughPercent.observe(viewLifecycleOwner) {
-            val color = getColorByState(it)
-            binding.barAnswersProgress.progressTintList = ColorStateList.valueOf(color)
-        }
-
-        viewModel.minPercent.observe(viewLifecycleOwner) {
-            binding.barAnswersProgress.secondaryProgress = it
         }
 
         viewModel.gameResult.observe(viewLifecycleOwner) {
@@ -98,8 +77,6 @@ class GameFragment : Fragment() {
 
     private fun showQuestion(it: Question) {
         with(binding) {
-            tvSum.text = it.sum.toString()
-            tvLeftNumber.text = it.visibleNumber.toString()
             for (i in 0 until tvOptions.size) {
                 tvOptions[i].text = it.options[i].toString()
             }
@@ -107,16 +84,11 @@ class GameFragment : Fragment() {
     }
 
     private fun launchResultFragment(result: GameResult) {
-        findNavController().navigate(GameFragmentDirections.actionGameFragmentToResultFragment(result))
-    }
-
-    private fun getColorByState(it: Boolean): Int {
-        val colorResId = if (it) {
-            android.R.color.holo_green_light
-        } else {
-            android.R.color.holo_red_light
-        }
-        return ContextCompat.getColor(requireContext(), colorResId)
+        findNavController().navigate(
+            GameFragmentDirections.actionGameFragmentToResultFragment(
+                result
+            )
+        )
     }
 
     private fun setOptionsClickListeners() {
